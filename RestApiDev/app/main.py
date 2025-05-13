2d88c78426f8f9d11b58ca974021d6fb433f8d1a
lenard0215 <scpalmer2677@gmail.com>
2025-05-13 08:49:03 -0700

    save files

--- RestApiDev/app/main.py
+++ RestApiDev/app/main.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app= FastAPI()
 
